954affc39aa020fddda053c98d6db67233282f5e
Trevor Norris <trev.norris@gmail.com>
2014-10-27 12:01:48 -0700

    Fix example to always use uint64_t
    For 32-bit builds the sum would wrap around and print a negative number.
    This has been fixed.

--- example/sporadic.c
+++ example/sporadic.c
@@ -7,6 +7,7 @@
 #include <pthread.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <inttypes.h>
 
 #define ITER 1e7
 
@@ -15,7 +16,7 @@ pthread_t thread;
 
 
 static void* task_runner(void* arg) {
-  size_t i;
+  uint64_t i;
 
   for (i = 1; i < ITER; i++) {
     fuq_push(&queue, (void*) i);
@@ -28,7 +29,7 @@ int main(void) {
   uint64_t sum = 0;
   uint64_t check = 0;
   void* tmp;
-  size_t i;
+  uint64_t i;
 
   fuq_init(&queue);
   assert(pthread_create(&thread, NULL, task_runner, NULL) == 0);
@@ -39,15 +40,15 @@ int main(void) {
       sum += (uint64_t) tmp;
   }
 
-  fprintf(stderr, "check: %zi\n", check);
-  fprintf(stderr, "sum:   %zi\n", sum);
+  fprintf(stderr, "check: %" PRIu64 "\n", check);
+  fprintf(stderr, "sum:   %" PRIu64 "\n", sum);
 
   assert(pthread_join(thread, NULL) == 0);
 
   while (!fuq_empty(&queue))
     sum += (uint64_t) fuq_shift(&queue);
 
-  fprintf(stderr, "sum:   %zi\n", sum);
+  fprintf(stderr, "sum:   %" PRIu64 "\n", sum);
   assert(sum == check);
 
   fuq_dispose(&queue);
