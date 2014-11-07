06e60cd3e334beb3eb47376c71a5825299d2f0cd
Trevor Norris <trev.norris@gmail.com>
2014-11-07 13:50:15 -0800

    Update example to print ops per sec
    Print out the number of push/shift per sec done during the loop.

--- example/sporadic.c
+++ example/sporadic.c
@@ -8,18 +8,44 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <inttypes.h>
+#include <time.h>
 
 #define ITER 1e7
+#define NS 1e9
+
+
+struct timespec ts_diff(struct timespec start, struct timespec end) {
+  struct timespec temp;
+  if ((end.tv_nsec - start.tv_nsec) < 0) {
+    temp.tv_sec = end.tv_sec - start.tv_sec - 1;
+    temp.tv_nsec = NS + (end.tv_nsec - start.tv_nsec);
+  } else {
+    temp.tv_sec = end.tv_sec - start.tv_sec;
+    temp.tv_nsec = end.tv_nsec - start.tv_nsec;
+  }
+  return temp;
+}
 
 
 static void* task_runner(void* arg) {
   fuq_queue* queue;
+  struct timespec ts_s;
+  struct timespec ts_f;
   uint64_t i;
+
+  clock_gettime(CLOCK_MONOTONIC, &ts_s);
 
   queue = (fuq_queue*) arg;
   for (i = 1; i < ITER; i++) {
     fuq_push(queue, (void*) i);
   }
+
+  clock_gettime(CLOCK_MONOTONIC, &ts_f);
+  ts_f= ts_diff(ts_s, ts_f);
+  fprintf(stderr,
+          "%zi push/sec\n",
+          (unsigned long) (ITER / (ts_f.tv_sec + ts_f.tv_nsec / NS)));
+
   return NULL;
 }
 
@@ -29,6 +55,8 @@ int main(void) {
   fuq_queue queue1;
   pthread_t thread0;
   pthread_t thread1;
+  struct timespec ts_s;
+  struct timespec ts_f;
   uint64_t sum0 = 0;
   uint64_t sum1 = 0;
   uint64_t check = 0;
@@ -40,6 +68,8 @@ int main(void) {
   assert(pthread_create(&thread0, NULL, task_runner, &queue0) == 0);
   assert(pthread_create(&thread1, NULL, task_runner, &queue1) == 0);
 
+  clock_gettime(CLOCK_MONOTONIC, &ts_s);
+
   for (i = 1; i < ITER; i++) {
     check += i;
     if (NULL != (tmp = fuq_shift(&queue0)))
@@ -48,12 +78,18 @@ int main(void) {
       sum1 += (uint64_t) tmp;
   }
 
-  fprintf(stderr, "check: %" PRIu64 "\n", check);
-  fprintf(stderr, "sum0:  %" PRIu64 "\n", sum0);
-  fprintf(stderr, "sum1:  %" PRIu64 "\n", sum1);
+  clock_gettime(CLOCK_MONOTONIC, &ts_f);
+  ts_f = ts_diff(ts_s, ts_f);
 
   assert(pthread_join(thread0, NULL) == 0);
   assert(pthread_join(thread1, NULL) == 0);
+
+  fprintf(stderr,
+          "%zi push/sec\n",
+          (unsigned long) (2 * ITER / (ts_f.tv_sec + ts_f.tv_nsec / NS)));
+  fprintf(stderr, "check: %" PRIu64 "\n", check);
+  fprintf(stderr, "sum0:  %" PRIu64 "\n", sum0);
+  fprintf(stderr, "sum1:  %" PRIu64 "\n", sum1);
 
   while (!fuq_empty(&queue0))
     sum0 += (uint64_t) fuq_shift(&queue0);
@@ -70,3 +106,6 @@ int main(void) {
 
   return 0;
 }
+
+#undef ITER
+#undef NS
